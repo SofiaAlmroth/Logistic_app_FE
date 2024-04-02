b7a167542565e762d0892e0f95c6a304669b655f
sofia_almroth <almrothsogfia@gmail.com>
2024-04-02 13:09:36 +0200

    finished searchbox ui

--- src/BalancePage.tsx
+++ src/BalancePage.tsx
@@ -3,6 +3,7 @@ import ListGroup from "./components/ListGroup";
 import { Category, getCategories } from "./services/fakeCategoryService";
 import { paints } from "./services/fakePaintService";
 import { Pagination } from "./components/Pagination";
+import SearchBox from "./components/SearchBox";
 
 const PAGE_SIZE = 4;
 const DEFAULT_CATEGORY: Category = {
@@ -17,30 +18,28 @@ function BalancePage() {
   ]);
 
   function handleCategoryToggle(category: Category, isChecked: boolean) {
-    setSelectedCategories((prevCategories) => {
-      let categories = prevCategories;
+    let categories = selectedCategories;
 
-      if (!isChecked) {
-        if (prevCategories.length === 1) {
-          categories = [DEFAULT_CATEGORY];
-        } else {
-          categories = prevCategories.filter((c) => c._id !== category._id);
-        }
+    if (!isChecked) {
+      if (selectedCategories.length === 1) {
+        categories = [DEFAULT_CATEGORY];
+      } else {
+        categories = selectedCategories.filter((c) => c._id !== category._id);
       }
+    }
 
-      if (isChecked) {
-        if (category._id === DEFAULT_CATEGORY._id) {
-          categories = [DEFAULT_CATEGORY];
-        } else {
-          categories = prevCategories.filter(
-            (c) => c._id !== DEFAULT_CATEGORY._id
-          );
-          categories.push(category);
-        }
+    if (isChecked) {
+      if (category._id === DEFAULT_CATEGORY._id) {
+        categories = [DEFAULT_CATEGORY];
+      } else {
+        categories = selectedCategories.filter(
+          (c) => c._id !== DEFAULT_CATEGORY._id
+        );
+        categories.push(category);
       }
+    }
 
-      return categories;
-    });
+    setSelectedCategories(categories);
   }
 
   if (paints.length === 0) return <p>There are no products in the database</p>;
@@ -55,53 +54,62 @@ function BalancePage() {
       );
 
   return (
-    <div className="flex">
-      <div className="flex-auto">
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Quantity</th>
-              <th>Price</th>
-              <th>SupplierInfo</th>
-              <th>Orderdate</th>
-              <th>EAN</th>
-              <th>Batch</th>
-              <th>Best before date</th>
-              <th />
-            </tr>
-          </thead>
-          <tbody>
-            {filteredPaints.map((paint) => (
-              <tr key={paint._id}>
-                <td>{paint.name}</td>
-                <td>{paint.quantity}</td>
-                <td>{paint.price}</td>
-                <td>{paint.supplierInfo}</td>
-                <td>{paint.orderDate.toLocaleDateString()}</td>
-                <td>{paint.EAN_GTIN}</td>
-                <td>{paint.batchName}</td>
-                <td>{paint.bestBeforeDate.toLocaleDateString()}</td>
-                <td>
-                  <button className="btn btn-ghost">Adjust</button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        <Pagination
-          totalCount={paints.length}
-          pageSize={PAGE_SIZE}
-          selectedPage={selectedPage}
-          onPageSelect={setSelectedPage}
-        />
+    <div className="container mx-auto">
+      <div className="flex justify-center">
+        <div className="w-1/2 m-6">
+          <SearchBox />
+        </div>
       </div>
-      <div className="flex-none ml-4">
-        <ListGroup
-          items={[DEFAULT_CATEGORY, ...getCategories()]}
-          selectedItems={selectedCategories}
-          onItemSelect={handleCategoryToggle}
-        />
+      <div className="flex justify-center">
+        <div className="flex">
+          <div className="mr-4">
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Quantity</th>
+                  <th>Price</th>
+                  <th>SupplierInfo</th>
+                  <th>Orderdate</th>
+                  <th>EAN</th>
+                  <th>Batch</th>
+                  <th>Best before date</th>
+                  <th />
+                </tr>
+              </thead>
+              <tbody>
+                {filteredPaints.map((paint) => (
+                  <tr key={paint._id}>
+                    <td>{paint.name}</td>
+                    <td>{paint.quantity}</td>
+                    <td>{paint.price}</td>
+                    <td>{paint.supplierInfo}</td>
+                    <td>{paint.orderDate.toLocaleDateString()}</td>
+                    <td>{paint.EAN_GTIN}</td>
+                    <td>{paint.batchName}</td>
+                    <td>{paint.bestBeforeDate.toLocaleDateString()}</td>
+                    <td>
+                      <button className="btn btn-ghost">Adjust</button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            <Pagination
+              totalCount={paints.length}
+              pageSize={PAGE_SIZE}
+              selectedPage={selectedPage}
+              onPageSelect={setSelectedPage}
+            />
+          </div>
+          <div className="">
+            <ListGroup
+              items={[DEFAULT_CATEGORY, ...getCategories()]}
+              selectedItems={selectedCategories}
+              onItemSelect={handleCategoryToggle}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
