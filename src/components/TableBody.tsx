f863abce832cbb4b0c438a18051ccffc85535943
atilla <atilla1993@hotmail.se>
2024-04-04 12:57:29 +0200

    klar med deletebutton och updatebutton, även lagt till lite animations

--- src/components/TableBody.tsx
+++ src/components/TableBody.tsx
@@ -1,14 +1,30 @@
 import { Paint } from "../services/fakePaintService";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPen, faTimes } from "@fortawesome/free-solid-svg-icons";
+import "../App.css";
+import { useState } from "react";
 
 interface Props {
   paints: Paint[];
+  onDelete(id: string): void;
 }
 
-export function TableBody({ paints }: Props) {
+export function TableBody({ paints, onDelete }: Props) {
+  const [slideOut, setSlideOut] = useState<string | null>(null);
+
+  const handleDelete = (id: string) => {
+    setSlideOut(id); // Set the ID to fade out
+    setTimeout(() => {
+      onDelete(id); // After a delay, trigger the delete action
+    }, 1000); // Adjust the delay time as needed
+  };
   return (
     <tbody>
       {paints.map((paint) => (
-        <tr key={paint._id}>
+        <tr
+          key={paint._id}
+          className={slideOut === paint._id ? "slide-out" : ""}
+        >
           <td>{paint.name}</td>
           <td>{paint.quantity}</td>
           <td>{paint.price}</td>
@@ -18,7 +34,21 @@ export function TableBody({ paints }: Props) {
           <td>{paint.batchName}</td>
           <td>{paint.bestBeforeDate.toLocaleDateString()}</td>
           <td>
-            <button className="btn btn-ghost">Update</button>
+            <div className="tooltip" data-tip="Update">
+              <button className="btn btn-circle">
+                <FontAwesomeIcon icon={faPen} />
+              </button>
+            </div>
+          </td>
+          <td>
+            <div className="tooltip tooltip-error" data-tip="Delete">
+              <button
+                className="btn btn-circle"
+                onClick={() => handleDelete(paint._id)}
+              >
+                <FontAwesomeIcon icon={faTimes} size="lg" />
+              </button>
+            </div>
           </td>
         </tr>
       ))}
