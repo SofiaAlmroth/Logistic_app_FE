b31f421d24149d29e8fd19ed4cc3f560c068d7e8
sofia_almroth <almrothsogfia@gmail.com>
2024-04-04 12:05:27 +0200

    added date picker, react hook form and zod validation

--- src/components/ProductModal.tsx
+++ src/components/ProductModal.tsx
@@ -1,25 +1,117 @@
-// import DatePicker from "react-datepicker";
-// import { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 import { useRef } from "react";
+import { getCategories } from "../services/fakeCategoryService";
+import { Controller, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const schema = z.object({
+  _id: z.string().optional(),
+  name: z.string().min(1, { message: "Name is required" }),
+  categoryId: z.string().min(1, { message: "Category is required" }),
+  quantity: z.coerce.number().gt(0, { message: "Quantity is required" }),
+  price: z.coerce.number().gt(0, { message: "Price is required" }),
+  supplierInfo: z.string().min(1, { message: "Name is required" }),
+  orderDate: z.coerce.date(),
+});
+
+type FormData = z.infer<typeof schema>;
 
 function ProductModal() {
   const modalRef = useRef<HTMLDialogElement>(null);
+  const { register, handleSubmit, control, reset } = useForm<FormData>({
+    resolver: zodResolver(schema),
+    mode: "onChange",
+  });
+
+  function handleDateChange(date: Date) {
+    console.log(date);
+  }
+  function onSubmit(data: FormData) {
+    console.log("data", data);
+    reset();
+    modalRef.current?.close();
+  }
 
   return (
     <>
       <button
         onClick={() => modalRef.current?.showModal()}
-        className="custom-button"
+        className="custom-button btn-wide"
       >
         New Order
       </button>
       <dialog id="modal" className="modal" ref={modalRef}>
-        <div className="modal-box">
-          <h3 className="font-bold text-lg">Hello!</h3>
-          <p className="py-4">Press ESC key or click outside to close</p>
-        </div>
+        <form className="modal-box" onSubmit={handleSubmit(onSubmit)}>
+          <h1 className="font-bold text-xl mb-3">New Order</h1>
+          <div>
+            <input
+              {...register("name")}
+              type="text"
+              placeholder="Product Name"
+              className="input input-bordered w-full mb-4"
+            />
+            <select
+              {...register("categoryId")}
+              defaultValue=""
+              className="select select-bordered w-full mb-4 text-base"
+            >
+              <option value={""} disabled>
+                Category
+              </option>
+
+              {getCategories().map((category) => (
+                <option key={category._id}>{category.name}</option>
+              ))}
+            </select>
+            <input
+              {...register("quantity")}
+              type="text"
+              placeholder="Quantity"
+              className="input input-bordered w-full mb-4"
+            />
+            <input
+              {...register("price")}
+              type="text"
+              placeholder="Price"
+              className="input input-bordered w-full mb-4"
+            />
+            <input
+              {...register("supplierInfo")}
+              type="text"
+              placeholder="SupplierInfo"
+              className="input input-bordered w-full mb-4"
+            />
+            <div className="mb-4 ">
+              <Controller
+                control={control}
+                name="orderDate"
+                render={({ field }) => (
+                  <DatePicker
+                    selected={field.value}
+                    onChange={(date: Date) => {
+                      field.onChange(date);
+                      handleDateChange(date);
+                    }}
+                    className="input input-bordered w-full"
+                    dateFormat="yyyy-MM-dd"
+                    placeholderText="Order Date"
+                    popperPlacement="bottom-start"
+                    wrapperClassName="w-full"
+                  />
+                )}
+              />
+            </div>
+          </div>
+          <div className="form-control">
+            <button type="submit" className="btn btn-primary mt-3">
+              Save
+            </button>
+          </div>
+        </form>
         <form method="dialog" className="modal-backdrop">
-          <button>close</button>
+          <button>Close</button>
         </form>
       </dialog>
     </>
@@ -27,13 +119,3 @@ function ProductModal() {
 }
 
 export default ProductModal;
-
-// const [startDate, setStartDate] = useState(new Date());
-
-// const handleDateChange = (date: Date) => {
-//   if (date !== null) {
-//     setStartDate(date);
-//   }
-// };
-
-// return <DatePicker selected={startDate} onChange={handleDateChange} />;
--- src/pages/OrderPage.tsx
+++ src/pages/OrderPage.tsx
@@ -2,7 +2,7 @@ import ProductModal from "../components/ProductModal";
 
 function OrderPage() {
   return (
-    <div className="text-center m-16">
+    <div className=" m-16">
       <ProductModal />
     </div>
   );
