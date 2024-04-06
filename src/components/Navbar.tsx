a3a8867f28322de443e7ebba723bf4694601fa65
Ammar <ammarelkhalil@outlook.com>
2024-04-07 01:31:09 +0200

    now able to login user

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,20 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
+import { User } from "../types";
 
 export default function Navbar() {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (!token) return;
+
+    const user = jwtDecode<User>(token);
+    setUser(user);
+  }, []);
+
   return (
     <div className="navbar bg-base-100 shadow mb-2">
       <div className="navbar-start">
@@ -21,12 +35,26 @@ export default function Navbar() {
           </li>
         </ul>
       </div>
-      <div className="navbar-end">
-        <a className="btn">Login</a>
-        <Link to="/register" className="btn btn-primary ml-2">
-          Register
-        </Link>
-      </div>
+      {user && (
+        <div className="navbar-end">
+          <Link to="/profile" className="btn btn-neutral ml-2">
+            {user.name}
+          </Link>
+          <Link to="/logout" className="btn btn-primary ml-2">
+            Logout
+          </Link>
+        </div>
+      )}
+      {!user && (
+        <div className="navbar-end">
+          <Link to="/login" className="btn btn-neutral ml-2">
+            Login
+          </Link>
+          <Link to="/register" className="btn btn-primary ml-2">
+            Register
+          </Link>
+        </div>
+      )}
     </div>
   );
 }
